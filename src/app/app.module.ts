71f851c9d36b5851268be5f89b45125d5b4b9933
erhic <ericngugi24@gmail.com>
2023-07-20 16:15:37 +0300

    feature: passing component as an export from  another module to main module, public API  doesnt expose an component to the module that isnt declared and imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ProductsModule } from './products/products.module';
 
-import { ProductComponent } from './product/product.component';
-
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    
   ],
   imports: [
     BrowserModule,
-    ProductsModule,
-    ProductComponent
+    ProductsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-// import { ProductListComponent } from '../product-list/product-list.component';
+import { ProductListComponent } from '../product-list/product-list.component';
 
 
 
 
 
 @NgModule({
-  declarations: [],
-    // ProductListComponent],
+  declarations: [
+    ProductListComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports:[ProductListComponent]
 })
 export class ProductsModule { }
