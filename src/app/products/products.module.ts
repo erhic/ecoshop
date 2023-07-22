5f150a0e53c0081452fd920d3ef3d1ef121bd0ec
erhic <ericngugi24@gmail.com>
2023-07-22 10:36:22 +0300

    feature eventbinding- added click event to be able to pass methods  from templates to component

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductListComponent } from '../product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 
 
@@ -9,7 +10,8 @@ import { ProductListComponent } from '../product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    ProductListComponent],
+    ProductListComponent,
+    ProductDetailComponent],
   imports: [
     CommonModule
   ],
