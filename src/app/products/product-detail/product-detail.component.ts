d9a6bb64f14d1a1569827214cce9c3d6e869dd10
erhic <ericngugi24@gmail.com>
2023-07-22 17:47:28 +0300

    feature: changestrategy,set up the component to onbounds once it is needed or change in data detection ,events occurs ,it called to execute unlike when there is no changedection startegy , it is runs first it when the component lifecycle is intialized and the second time when a there is a change detection

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit,Input,EventEmitter, Output,ViewEncapsulation } from '@angular/core';
+import { Component, OnInit,Input,EventEmitter, Output,ViewEncapsulation,ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
 export class ProductDetailComponent implements OnInit {
 @Input() name=""
@@ -15,5 +16,10 @@ export class ProductDetailComponent implements OnInit {
   }
   buy(){
    this.bought.emit(this.name) 
+  }
+  get productName():string{
+    console.log(`get ${this.name}`)
+    return this.name
+
   }
 }
