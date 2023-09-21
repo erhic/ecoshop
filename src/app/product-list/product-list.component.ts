93e2711951bb3aafeaca569ae899153b207359be
erhic <ericngugi24@gmail.com>
2023-09-21 19:26:24 +0300

    feat: added responsive cards

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -16,16 +16,19 @@ currentStyles={
 }
 
 selectorProduct=[
-  {name:'Webcam',price:100},
-  {name:'Mic',price:300},
-  {name:'Ear pods',price:200},
-  {name:'Guitar',price:400},
-  {name:'Xolophone',price:500},
-  {name:'Piano',price:600},
+  {name:'Webcam',price:100, image:'https://www.camerasafrica.com/wp-content/uploads/2020/07/Untitled.jpg'},
+  {name:'Mic',price:300, image:'https://www.camerasafrica.com/wp-content/uploads/2020/10/BOYA-BY-PM700-280.jpg'},
+  {name:'Ear pods',price:200 ,image:'https://www.camerasafrica.com/wp-content/uploads/2022/09/Sony-WF-L900-LinkBuds-True-Wireless-Open-Earbuds-Dark-Gray-2-camerasafrica.jpg'},
+  {name:'Guitar',price:400 , image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXcuUIaOspMRT42yzGBUYx8n_wxSN59mcDRl_tYNG1FMtsw7-4ajzUUpLFCrAEtxb-7VQ&usqp=CAU'},
+  {name:'Xylophone',price:500,image:'https://m.media-amazon.com/images/I/61VdSDWyrNL._AC_UL320_.jpg'},
+  {name:'Piano',price:600, image:'https://m.media-amazon.com/images/I/71Kq41O6MTL._AC_UL320_.jpg'},
 ]
 
+/**  this method below is called in template view of this component , if it evaluates true, the css class from the styling file of this component in the added else removed if evaluation is false.
+ * 
+*/
 getToPassCssFromHere(){
-  return this.selectorProduct?this.selectorProduct.length>=19:undefined
+  return this.selectorProduct?this.selectorProduct.length>=1:undefined
  
 }
 
