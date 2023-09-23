07418bf7289c5d590d4b395fed2a2281aa85c505
erhic <ericngugi24@gmail.com>
2023-09-23 20:10:43 +0300

    feat; created side nav bar

--- src/app/shared-features/shared-features.module.ts
+++ src/app/shared-features/shared-features.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    SideNavComponent
   ],
   imports: [
     CommonModule
